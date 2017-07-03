82e285669adb11b495686f54b5b41088dd37e3f0
Campbell Graham <Campbell Graham>
2017-07-03 20:55:32 +1000

    Added session storage, other minor changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
 var app = angular.module("myApp", []);
 app.controller("myCtrl", function ($scope) {
-    $scope.players = [];
+
+    if (sessionStorage.getItem("players") === null) {
+        $scope.players = [];
+    }
+    else {
+        $scope.players = JSON.parse(sessionStorage.getItem("players"));
+    }
+
     $scope.playerName;
 
     $scope.addPlayer = function () {
         var newPlayer = { name: $scope.playerName, life: 40 };
         $scope.players.push(newPlayer);
         $scope.playerName = "";
+        $scope.savePlayers();
+        document.getElementById("playerNameInput").focus();
     }
 
     $scope.deletePlayer = function (playertoDelete) {
         var index = $scope.players.indexOf(playertoDelete);
         $scope.players.splice(index, 1);
+        $scope.savePlayers();
+    }
+
+    $scope.savePlayers = function() {
+        sessionStorage.setItem("players", JSON.stringify($scope.players));
     }
 });
 
