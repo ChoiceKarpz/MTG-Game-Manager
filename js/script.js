2d8ed85927975b30e9f21cd69516897f99bc6c2d
Campbell Graham <Campbell Graham>
2017-07-03 19:46:36 +1000

    Various changes and bug fixes, including allowing users to enter a player name for each player

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 var app = angular.module("myApp", []);
 app.controller("myCtrl", function ($scope) {
-    $scope.players = [
-        { name: 'Campbell', life: 0 },
-        { name: 'Ben', life: 0 },
-        { name: 'Dylan', life: 0 },
-        { name: 'Blake', life: 0 }
-    ];
+    $scope.players = [];
+    $scope.playerName;
+
+    $scope.addPlayer = function () {
+        var newPlayer = { name: $scope.playerName, life: 40 };
+        $scope.players.push(newPlayer);
+        $scope.playerName = "";
+    }
+
+    $scope.deletePlayer = function (playertoDelete) {
+        var index = $scope.players.indexOf(playertoDelete);
+        $scope.players.splice(index, 1);
+    }
+});
+
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+    $('.modal').modal();
 });
