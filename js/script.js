92bb44b8a09bdda9f66f040e6289572ed29d36c8
Campbell Graham <Campbell Graham>
2017-07-06 01:15:49 +1000

    Commander damage functioning, however most likely has some undiscovered issues

--- js/script.js
+++ js/script.js
@@ -11,11 +11,23 @@ app.controller("myCtrl", function ($scope) {
     $scope.playerName;
 
     $scope.addPlayer = function () {
-        var newPlayer = { name: $scope.playerName, life: 40 };
+        var newPlayer = { name: $scope.playerName, life: 40, damage: [{name: '', damageReceived: 0}] };
+
+        for (var i = 0; i < $scope.players.length; i++)
+            newPlayer.damage.push({ name: $scope.players[i].name, damageReceived: 0 });
+
+        alert(newPlayer.damage);
+
         $scope.players.push(newPlayer);
         $scope.playerName = "";
         $scope.savePlayers();
         $scope.setNameInputFocus();
+
+        for (var i = 0; i < $scope.players.length; i++) {
+            if ($scope.players[i] != newPlayer) {
+                $scope.players[i].damage.push({ name: newPlayer.name, damageReceived: 0 });
+            }
+        }
     }
 
 
@@ -40,6 +52,13 @@ app.controller("myCtrl", function ($scope) {
     $scope.adjustPlayerLife = function (playertoAlter, difference) {
         var index = $scope.players.indexOf(playertoAlter);
         $scope.players[index].life += difference;
+        $scope.savePlayers();
+    }
+
+    $scope.adjustCommanderDamage = function (defendingPlayer, attackingPlayer, difference) {
+        var indexDefender = $scope.players.indexOf(defendingPlayer);
+        var indexAttacker = $scope.players[indexDefender].damage.indexOf(attackingPlayer);
+        $scope.players[indexDefender].damage[indexAttacker].damageReceived += difference;
         $scope.savePlayers();
     }
 });
